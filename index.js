dc40b1dd304bd36b4416c598b5ee650aaa53f041
Paul HALLEUX <pa.halleux@student.helmo.be>
2022-03-19 13:09:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint"
+    ],
+    rules: {
+        indent: [
+            "warn",
+            "tab"
+        ],
+        'linebreak-style': [
+            "warn",
+            "windows"
+        ],
+        quotes: [
+            "warn",
+            "single"
+        ],
+        semi: [
+            "warn",
+            "always"
+        ],
+        'keyword-spacing': [
+            "warn",
+            {
+                "before": true
+            }
+        ],
+        'no-multi-spaces': [
+            "warn"
+        ],
+        'no-trailing-spaces': [
+            "warn"
+        ],
+        'object-curly-spacing': [
+            "warn",
+            "always"
+        ],
+        'react/jsx-wrap-multilines': [
+            "error",
+            {
+                "declaration": "parens-new-line",
+                "assignment": "parens-new-line",
+                "return": "parens-new-line",
+                "arrow": "parens-new-line",
+                "condition": "parens-new-line",
+                "logical": "parens-new-line",
+                "prop": "parens-new-line"
+            }
+        ],
+        'no-empty-pattern': "off",
+        '@typescript-eslint/no-explicit-any': "off",
+        'react/jsx-first-prop-new-line': [
+            "warn",
+            "multiline"
+        ]
+    }
+}
